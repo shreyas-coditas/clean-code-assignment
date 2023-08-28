b52f4feeeeadefd21dc0a240f6ddb4dbce7bbcef
shreyas-coditas <78950545+shreyas-coditas@users.noreply.github.com>
2023-08-28 12:26:27 +0530

    Clean Code File

--- CleanCode.js
+++ CleanCode.js
@@ -5,7 +5,7 @@ class Book {
     this.publicationYear = publicationYear;
   }
 
-  updateDetails() {
+  updateDetails(title, author, publicationYear) {
     if (title !== undefined) {
       this.title = title;
     }
@@ -13,7 +13,11 @@ class Book {
       this.author = author;
     }
     if (publicationYear !== undefined) {
-      this.publicationYear = publicationYear;
+      if (Number.isInteger(publicationYear) && publicationYear >= 0) {
+        this.publicationYear = publicationYear;
+      } else {
+        throw new Error('Invalid publication year.');
+      }
     }
   }
 }
@@ -24,7 +28,11 @@ class Library {
   }
 
   addBook(book) {
-    this.books.push(book);
+    if (book instanceof Book) {
+      this.books.push(book);
+    } else {
+      throw new Error('Invalid book object.');
+    }
   }
 
   searchByTitle(title) {
@@ -40,14 +48,20 @@ class Library {
   }
 }
 
-//Example
-const book1 = new Book('The Song of Ice and Fire', 'George R.R. Martin', 2009);
-const book2 = new Book('The Intelligent Investor', 'Benjamin Franklin', 1959);
+// Example
+try {
+  const book1 = new Book('The Song of Ice and Fire', 'George R.R. Martin', 2009);
+  const book2 = new Book('The Intelligent Investor', 'Benjamin Graham', 1959);
 
-const library = new Library();
-library.addBook(book1);
-library.addBook(book2);
+  const library = new Library();
+  library.addBook(book1);
+  library.addBook(book2);
 
-console.log('Book by Title', library.searchByTitle('The Intelligent Investor'));
-console.log('Total Books', library.getTotalBooks());
-console.log('Book by Author', library.searchByAuthor('George R.R. Martin'));
+  console.log('Book by Title:', library.searchByTitle('The Intelligent Investor'));
+  console.log('Total Books:', library.getTotalBooks());
+  console.log('Book by Author:', library.searchByAuthor('George R.R. Martin'));
+
+  book1.updateDetails(undefined, undefined, -100); // Invalid publication year
+} catch (error) {
+  console.error('An error occurred:', error.message);
+}
